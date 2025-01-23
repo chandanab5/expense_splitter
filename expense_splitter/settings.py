e56405f899fa6f08c3d3b759e510c1e01781ee89
chandanab5 <baddireddysrichandana3@gmail.com>
2025-01-23 14:49:27 +0530

    Updated permission for register view and changed settings

--- expense_splitter/settings.py
+++ expense_splitter/settings.py
@@ -58,9 +58,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
 }
 
 ROOT_URLCONF = 'expense_splitter.urls'
--- split_expense/views.py
+++ split_expense/views.py
@@ -1,5 +1,5 @@
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated , AllowAny
 from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth.models import User
@@ -8,6 +8,7 @@ from .serializers import UserSerializer, ExpenseGroupSerializer, ExpenseSerializ
 
 
 @api_view(['POST'])
+@permission_classes([AllowAny])
 def register(request):
     username = request.data.get('username')
     password = request.data.get('password')
